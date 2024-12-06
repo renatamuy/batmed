538454deb47d26bbc43fe0b3ad0c66b167c66cd1
Renata Muylaert <renatamuy@gmail.com>
2024-12-06 16:04:18 +1300

    code

--- code/batmed_networks.R
+++ code/batmed_networks.R
@@ -1,15 +1,16 @@
-# Tackett Data:
+# Tackett Data in networks
 # https://www.mdpi.com/article/10.3390/d14030179/s1. 
-
-#Dataviz ideas: https://github.com/manlius/muxViz/blob/master/gui-old/theory/README.md
-
+# Dataviz ideas: https://github.com/manlius/muxViz/blob/master/gui-old/theory/README.md
 
 require(here)
-#remotes::install_github("gastonstat/arcdiagram")
 library(dplyr)
 library(ggraph)
-
-require(arcdiagram)
+require(networkD3)
+require(echarts4r)
+devtools::install_github('Ecological-Complexity-Lab/emln', force=T) # new package - explore maybe
+library(emln)
+# install.packages("ggalluvial")
+library(ggalluvial)
 if (!require(tidyverse)) install.packages('tidyverse')
 if (!require(ggplot2)) install.packages('ggplot2')
 if (!require(xlsx)) install.packages('xlsx')
@@ -18,18 +19,17 @@ if (!require(igraph)) install.packages('igraph')
 setwd(here())
 setwd('data')
 
-
 df <- read.xlsx('Supplementary Materials-Table S3.xlsx', sheetIndex = 1, startRow=2)
 
 colnames(df)
 
+# Layers to explore 
 df[c('species', 'body.part', 'cures'  )]
 
 unique(df$IUCN.Region)
 unique(df$species)
 unique(df$body.part)
 unique(df$cures)
-
 
 # Prep
 
@@ -42,7 +42,6 @@ df <- df %>%
 df <- df %>%
   mutate(across(everything(), ~ gsub("^unknown\\s*$", "unknown", .)))
 
-
 links <- df %>%
   count(species, body.part, cures) %>%
   rename(value = n) %>%
@@ -51,37 +50,21 @@ links <- df %>%
 
 links
 
-# Extract unique nodes
 nodes <- unique(c(links$source, links$target))
 nodes <- data.frame(name = nodes)
 
-# Output nodes and links
 head(nodes)
 head(links)
+
+# Plot a cool sankey graph 
 
 links %>%
   e_charts() %>%
   e_sankey(links, source = source, target = target, value = value) %>%
   e_theme("macarons")
 
-str(links)
 
-
-# Multilayer -----------------
-
-devtools::install_github('Ecological-Complexity-Lab/emln', force=T)
-
-library(emln)
-library(dplyr)
-
-# Install necessary packages if not already installed
-# install.packages("ggplot2")
-# install.packages("ggalluvial")
-
-library(ggplot2)
-library(ggalluvial)
-library(dplyr)
-
+# Multilayer attempts -----------------
 
 ggplot(data = df,
        aes(axis1 = species, axis2 = body.part, axis3 = cures)) +
@@ -94,3 +77,4 @@ ggplot(data = df,
   theme_minimal()
 
 
+#-------------------------------------------------------------------------------------------------------------
